da7669d4e5dc2cfb13ffe043512fce0d06b0b436
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-04 03:37:17 +0200

    Created constants file and added constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppMeasure {
+  static late double width;
+  static late double height;
+}
+
+class Color {
+  static late Color backgroundColor;
+  static late Color dodgerBlue;
+  static late Color darkBlue;
+}
