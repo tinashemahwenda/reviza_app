21560fc5811d3d9f4425aecd646c99658e4194fc
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-04 04:54:06 +0200

    Created a navigation bar

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -8,10 +8,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.backgroundColor,
-      body: Center(
-        child: Text('Hello Reviza App'),
-      ),
-    );
+        backgroundColor: AppColor.backgroundColor,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/images/reviza-transparent-long.png',
+                      width: 60,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        color: AppColor.dodgerBlue,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
