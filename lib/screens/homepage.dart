c338cc8f269eb92313b300b99e8f44f44afeb809
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-04 04:45:39 +0200

    Set background color

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../constants/app_constants.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.dodgerBlue,
       body: Center(
         child: Text('Hello Reviza App'),
       ),
