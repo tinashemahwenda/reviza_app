05057d8844d22f5cf22e9f59e06cc1d40e70b366
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-04 04:53:49 +0200

    Made a cool navigation bar

---
+++ lib/widgets/navigationbar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_constants.dart';
+
+class RevizaNavigation extends StatelessWidget {
+  const RevizaNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Image.asset(
+          'assets/images/reviza-transparent-long.png',
+          width: 60,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            color: AppColor.dodgerBlue,
+          ),
+        )
+      ],
+    );
+  }
+}
