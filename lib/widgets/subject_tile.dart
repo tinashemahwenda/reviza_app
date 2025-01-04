115639ea8f112288b85237faccdb83fb059d6e27
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-04 05:40:46 +0200

    Created a seperate file for subject tile

---
+++ lib/widgets/subject_tile.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_constants.dart';
+
+class SubjectTile extends StatelessWidget {
+  const SubjectTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          width: AppMeasure.width,
+          height: AppMeasure.height / 7,
+          color: Colors.white,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    color: AppColor.darkBlue,
+                    child: Image.asset(
+                      'assets/images/reviza-icon.png',
+                      width: 60,
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                'Physics',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Manrope-Extrabold',
+                    fontSize: 18),
+              ),
+              Expanded(child: SizedBox()),
+              Icon(Icons.arrow_forward_ios),
+              SizedBox(
+                width: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
