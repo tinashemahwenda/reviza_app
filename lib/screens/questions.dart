dda2405a255fd470d5100e4c4e09af52c48525bb
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 07:26:59 +0200

    Made a back button

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+import '../constants/app_constants.dart';
+
 class QuestionsPage extends StatelessWidget {
   const QuestionsPage({super.key});
 
@@ -10,15 +12,33 @@ class QuestionsPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Center(
         child: Column(
+          spacing: 20,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Lottie.asset('assets/images/animations/loading.json', width: 200),
             SizedBox(height: 20),
             Text(
-              'Making Questions',
+              'Still uploading questions',
               style: TextStyle(
                 fontFamily: 'Manrope-Extrabold',
                 fontSize: 20,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  color: AppColor.darkBlue,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Go Back',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
             )
           ],
