5f80b74320b11583a90801152de53a4ea627960f
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 15:51:53 +0200

    Refactored questions page

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reviza_app/constants/app_constants.dart';
+import 'package:reviza_app/widgets/questions_bubble.dart';
 import 'package:reviza_app/widgets/questions_navbar.dart';
 
 class QuestionsPage extends StatelessWidget {
@@ -23,56 +24,7 @@ class QuestionsPage extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  width: AppMeasure.width,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Question 1',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Manrope-Extrabold',
-                            ),
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              color: Colors.blueAccent,
-                              child: Text(
-                                '0625/33/M/J/23',
-                                style: TextStyle(
-                                  fontSize: 8,
-                                  fontFamily: 'Manrope-Extrabold',
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          '1 (a) A boat crosses a river. The boat points at right angles to the river bank and it travels at a speed of 3.5m/ s relative to the water.A river current acts at right angles to the direction the boat points. The river current has a speed of 2.5m/s.By drawing a scale diagram or by calculation, determine the speed and direction of the boat relative to the river bank',
-                          style: TextStyle(
-                            height: 2,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
+              QuestionsBubble()
             ],
           )),
         ));
