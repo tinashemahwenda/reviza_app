a71497643282a6689956addddba1500fa7d4f4b7
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 07:06:26 +0200

    Added a loader to the questions page

---
+++ lib/screens/questions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class QuestionsPage extends StatelessWidget {
+  const QuestionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [Lottie.asset('assets/images/animations/loading.json')],
+        ),
+      ),
+    );
+  }
+}
