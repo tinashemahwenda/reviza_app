b40438da47b5c66d84f35a5c3d3cc010aa774931
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 13:24:26 +0200

    Removed unused code

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -8,25 +8,7 @@ class QuestionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          spacing: 20,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset('assets/images/animations/loading.json', width: 200),
-            SizedBox(height: 20),
-            Text(
-              'Still uploading questions',
-              style: TextStyle(
-                fontFamily: 'Manrope-Extrabold',
-                fontSize: 20,
-              ),
-            ),
-            GoBackButton()
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(child: Column()));
   }
 }
