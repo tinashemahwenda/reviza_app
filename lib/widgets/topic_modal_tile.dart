0b79b00bc33caaa92e545e74ab58447aaed48734
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 06:54:22 +0200

    Created a modal tile

---
+++ lib/widgets/topic_modal_tile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_constants.dart';
+
+class TopicModalTile extends StatelessWidget {
+  const TopicModalTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+            width: AppMeasure.width,
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Center(child: Text('Motion, forces & Energy'))),
+      ),
+    );
+  }
+}
