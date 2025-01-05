a0a2fb5c82d5236e2e1f443b8a2ca9ff65f0a409
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 15:50:13 +0200

    Created a cool view for the questions

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -23,51 +23,54 @@ class QuestionsPage extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              Container(
-                padding: EdgeInsets.all(20),
-                width: AppMeasure.width,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Question 1',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Manrope-Extrabold',
-                          ),
-                        ),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            color: Colors.blueAccent,
-                            child: Text(
-                              '0625/33/M/J/23',
-                              style: TextStyle(
-                                fontSize: 8,
-                                fontFamily: 'Manrope-Extrabold',
-                              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  width: AppMeasure.width,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Question 1',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Manrope-Extrabold',
                             ),
                           ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        '1 (a) A boat crosses a river. The boat points at right angles to the river bank and it travels at a speed of 3.5m/ s relative to the water.A river current acts at right angles to the direction the boat points. The river current has a speed of 2.5m/s.By drawing a scale diagram or by calculation, determine the speed and direction of the boat relative to the river bank',
-                        style: TextStyle(
-                          height: 2,
-                        ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: EdgeInsets.all(5),
+                              color: Colors.blueAccent,
+                              child: Text(
+                                '0625/33/M/J/23',
+                                style: TextStyle(
+                                  fontSize: 8,
+                                  fontFamily: 'Manrope-Extrabold',
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          '1 (a) A boat crosses a river. The boat points at right angles to the river bank and it travels at a speed of 3.5m/ s relative to the water.A river current acts at right angles to the direction the boat points. The river current has a speed of 2.5m/s.By drawing a scale diagram or by calculation, determine the speed and direction of the boat relative to the river bank',
+                          style: TextStyle(
+                            height: 2,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               )
             ],
