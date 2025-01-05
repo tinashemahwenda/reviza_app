be0dd3d8d012409cb87a8293bdda750f4ff099d5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 15:59:40 +0200

    Made the questions bubble dynamic

--- lib/widgets/questions_bubble.dart
+++ lib/widgets/questions_bubble.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import '../constants/app_constants.dart';
 
 class QuestionsBubble extends StatelessWidget {
-  const QuestionsBubble({super.key});
+  final String questionBody;
+  final int questionNumber;
+  const QuestionsBubble(
+      {super.key, required this.questionNumber, required this.questionBody});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +24,7 @@ class QuestionsBubble extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    'Question 1',
+                    'Question ${questionNumber.toString()}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Manrope-Extrabold',
@@ -49,7 +52,7 @@ class QuestionsBubble extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Text(
-                  '1 (a) A boat crosses a river. The boat points at right angles to the river bank and it travels at a speed of 3.5m/ s relative to the water.A river current acts at right angles to the direction the boat points. The river current has a speed of 2.5m/s.By drawing a scale diagram or by calculation, determine the speed and direction of the boat relative to the river bank',
+                  questionBody,
                   style: TextStyle(
                     height: 2,
                   ),
