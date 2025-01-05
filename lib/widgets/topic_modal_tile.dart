ebd3187e64a1d051419c5a8c04b52fab892097f5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-05 07:00:44 +0200

    Reduced padding on tiles

--- lib/widgets/topic_modal_tile.dart
+++ lib/widgets/topic_modal_tile.dart
@@ -9,7 +9,7 @@ class TopicModalTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(2.0),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Container(
