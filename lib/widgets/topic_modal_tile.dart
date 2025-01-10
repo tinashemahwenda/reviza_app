648918d3484409379a538c6408094256c600eefa
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-11 01:06:05 +0200

    Removed badly placed gesture detector widget

--- lib/widgets/topic_modal_tile.dart
+++ lib/widgets/topic_modal_tile.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:reviza_app/screens/questions.dart';
 
 import '../constants/app_constants.dart';
+//import '../models/question.dart';
 
 class TopicModalTile extends StatelessWidget {
   final String topicTitle;
-  const TopicModalTile({super.key, required this.topicTitle});
+
+  const TopicModalTile({
+    super.key,
+    required this.topicTitle,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.push(
-          context, MaterialPageRoute(builder: (context) => QuestionsPage())),
-      child: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-              width: AppMeasure.width,
-              padding: EdgeInsets.all(18),
-              color: Colors.white,
-              child: Center(child: Text(topicTitle))),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+            width: AppMeasure.width,
+            padding: EdgeInsets.all(18),
+            color: Colors.white,
+            child: Center(child: Text(topicTitle))),
       ),
     );
   }
