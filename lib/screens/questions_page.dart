016588891dc7a1de3f45cc1ffe5d31a55fc62ecd
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-11 01:27:34 +0200

    Made topic display dynamic

--- lib/screens/questions_page.dart
+++ lib/screens/questions_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reviza_app/constants/app_constants.dart';
 import 'package:reviza_app/widgets/questions_bubble.dart';
-import 'package:reviza_app/widgets/questions_navbar.dart';
 
 import '../models/question.dart';
 
@@ -28,13 +27,46 @@ class _QuestionsPageState extends State<QuestionsPage> {
               ),
               GestureDetector(
                   onTap: () => Navigator.pop(context),
-                  child: RevizaQuestionsNav()),
+                  child: Row(
+                    children: [
+                      Icon(Icons.arrow_back_ios),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'CAMBRIDGE | IGCSE | O LEVEL | PHYSICS',
+                            style: TextStyle(
+                              fontSize: 10,
+                            ),
+                          ),
+                          Text(
+                            widget.topic.topic,
+                            style: TextStyle(
+                              fontFamily: 'Manrope-Extrabold',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
               SizedBox(
                 height: 30,
               ),
-              QuestionsBubble(
-                questionNumber: 1,
-                questionBody: widget.topic.questions[0].questionBody,
+              SizedBox(
+                width: AppMeasure.width,
+                height: AppMeasure.height,
+                child: ListView.builder(
+                    itemCount: widget.topic.questions.length,
+                    itemBuilder: (context, index) => QuestionsBubble(
+                          questionNumber: widget.topic.questions[index].id,
+                          questionBody:
+                              widget.topic.questions[index].questionBody,
+                        )),
               ),
             ],
           )),
