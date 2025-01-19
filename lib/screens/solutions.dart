da0b2ab6aac58b30d8b0f3b21cc6c812e3a5af9c
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-19 17:29:48 +0200

    Created a solutions page

---
+++ lib/screens/solutions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:reviza_app/widgets/uploading_questions_screen.dart';
+
+class SolutionsPage extends StatelessWidget {
+  const SolutionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: UploadingQuestionsScreen());
+  }
+}
