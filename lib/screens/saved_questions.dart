8303c1b5533e8808dd6ed755fa4eef5b540f2604
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-20 16:19:50 +0200

    Set color to white

--- lib/screens/saved_questions.dart
+++ lib/screens/saved_questions.dart
@@ -7,6 +7,7 @@ class SavedQuestions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: UploadingQuestionsScreen(uploadType: 'saved questions'),
     );
   }
