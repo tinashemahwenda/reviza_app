1d91ec7613bb32f063e49b02d31d9bcf564be043
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-21 16:31:17 +0200

    Created a page to handle user registration

---
+++ lib/screens/register_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Register'),
+        ],
+      ),
+    );
+  }
+}
