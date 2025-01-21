391a69b5ee6bea3a4ff482c8d65c2c884a36c246
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-21 16:36:40 +0200

    Made some routing for the register page

--- lib/widgets/navigationbar.dart
+++ lib/widgets/navigationbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reviza_app/screens/register_page.dart';
 import 'package:reviza_app/screens/saved_questions.dart';
 
 import '../constants/app_constants.dart';
@@ -11,9 +12,13 @@ class RevizaNavigation extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Image.asset(
-          'assets/images/reviza-transparent-long.png',
-          width: 100,
+        GestureDetector(
+          onTap: () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) => RegisterPage())),
+          child: Image.asset(
+            'assets/images/reviza-transparent-long.png',
+            width: 100,
+          ),
         ),
         GestureDetector(
           onTap: () => Navigator.push(context,
