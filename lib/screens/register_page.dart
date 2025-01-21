18d0853ff9af92d16f171152130df920157df67b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-21 17:05:16 +0200

    Added controller for Name and Surname

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -9,120 +9,128 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>;
+  final _nameSurnameController = TextEditingController();
+  final _schoolNameController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Form(
-              child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 100,
-              ),
-              Center(
-                child: Image.asset(
-                  'assets/images/reviza-transparent-long.png',
-                  width: 200,
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Form(
+                child: Column(
+              //crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 100,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Text('Register new account'),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    focusColor: AppColor.darkBlue,
-                    fillColor: AppColor.darkBlue,
-                    hoverColor: AppColor.darkBlue,
-                    label: Text(
-                      'Full Name',
-                      style: TextStyle(
-                        fontSize: 14,
+                Center(
+                  child: Image.asset(
+                    'assets/images/reviza-transparent-long.png',
+                    width: 200,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text('Register new account'),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    controller: _nameSurnameController,
+                    decoration: InputDecoration(
+                      focusColor: AppColor.darkBlue,
+                      fillColor: AppColor.darkBlue,
+                      hoverColor: AppColor.darkBlue,
+                      label: Text(
+                        'Full Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
                       ),
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    } else {
-                      return null;
-                    }
-                  },
                 ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    focusColor: AppColor.darkBlue,
-                    fillColor: AppColor.darkBlue,
-                    hoverColor: AppColor.darkBlue,
-                    label: Text(
-                      'School Name',
-                      style: TextStyle(
-                        fontSize: 14,
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      focusColor: AppColor.darkBlue,
+                      fillColor: AppColor.darkBlue,
+                      hoverColor: AppColor.darkBlue,
+                      label: Text(
+                        'School Name',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
                       ),
                     ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your school name';
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your school name';
-                    } else {
-                      return null;
-                    }
-                  },
                 ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    focusColor: AppColor.darkBlue,
-                    fillColor: AppColor.darkBlue,
-                    hoverColor: AppColor.darkBlue,
-                    label: Text(
-                      'Phone Number',
-                      style: TextStyle(
-                        fontSize: 14,
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      focusColor: AppColor.darkBlue,
+                      fillColor: AppColor.darkBlue,
+                      hoverColor: AppColor.darkBlue,
+                      label: Text(
+                        'Phone Number',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
                       ),
                     ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your phone number';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: AppColor.darkBlue),
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your phone number';
+                      } else {
+                        return null;
+                      }
+                    },
                   ),
                 ),
-              )
-            ],
-          )),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColor.darkBlue),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            )),
+          ),
         ));
   }
 }
