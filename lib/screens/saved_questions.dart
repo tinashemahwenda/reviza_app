a66edcb74473f8768c3b321f8cf02643cb640d99
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-29 08:55:14 +0200

    Converted saved page to Stateful Widget

--- lib/screens/saved_questions.dart
+++ lib/screens/saved_questions.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:reviza_app/widgets/uploading_questions_screen.dart';
 
-class SavedQuestions extends StatelessWidget {
+class SavedQuestions extends StatefulWidget {
   const SavedQuestions({super.key});
 
+  @override
+  State<SavedQuestions> createState() => _SavedQuestionsState();
+}
+
+class _SavedQuestionsState extends State<SavedQuestions> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
