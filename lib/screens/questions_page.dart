6033d0079a6a881e49f6fe1d4475af780aa7a5db
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-05 09:06:21 +0200

    Removed FAB

--- lib/screens/questions_page.dart
+++ lib/screens/questions_page.dart
@@ -74,139 +74,123 @@ class _QuestionsPageState extends State<QuestionsPage> {
       ..sort();
 
     return Scaffold(
-        backgroundColor: AppColor.backgroundColor,
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-              GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Row(
-                    children: [
-                      Icon(Icons.arrow_back_ios),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'CAMBRIDGE | IGCSE | O LEVEL | PHYSICS',
-                            style: TextStyle(
-                              fontSize: 10,
-                            ),
+      backgroundColor: AppColor.backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Row(
+                  children: [
+                    Icon(Icons.arrow_back_ios),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'CAMBRIDGE | IGCSE | O LEVEL | PHYSICS',
+                          style: TextStyle(
+                            fontSize: 10,
                           ),
-                          Text(
-                            widget.topic.topic,
-                            style: TextStyle(
-                              fontFamily: 'Manrope-Extrabold',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
+                        ),
+                        Text(
+                          widget.topic.topic,
+                          style: TextStyle(
+                            fontFamily: 'Manrope-Extrabold',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    SavedQuestionsButton()
+                  ],
+                )),
+            Padding(
+                padding: const EdgeInsets.only(bottom: 30, top: 20),
+                child: Wrap(
+                  spacing: 5,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    FilterChip(
+                      label: Text(
+                        'All',
+                        style: TextStyle(
+                          fontSize: 11,
+                          color: selectedFilter == 'All'
+                              ? Colors.white
+                              : AppColor.darkBlue,
+                        ),
                       ),
-                      Spacer(),
-                      SavedQuestionsButton()
-                    ],
-                  )),
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 30, top: 20),
-                  child: Wrap(
-                    spacing: 5,
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      FilterChip(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                      selected: selectedFilter == 'All',
+                      onSelected: (_) => filterQuestions('All'),
+                      showCheckmark: false,
+                      selectedColor: AppColor.darkBlue,
+                      backgroundColor: Colors.grey[200],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                    ),
+                    ...years.map((year) {
+                      return FilterChip(
                         label: Text(
-                          'All',
+                          '20$year',
                           style: TextStyle(
                             fontSize: 11,
-                            color: selectedFilter == 'All'
+                            color: selectedFilter == year
                                 ? Colors.white
                                 : AppColor.darkBlue,
                           ),
                         ),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                        selected: selectedFilter == 'All',
-                        onSelected: (_) => filterQuestions('All'),
-                        showCheckmark: false,
+                        selected: selectedFilter == year,
+                        onSelected: (_) => filterQuestions(year),
                         selectedColor: AppColor.darkBlue,
+                        showCheckmark: false,
                         backgroundColor: Colors.grey[200],
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(50)),
-                      ),
-                      ...years.map((year) {
-                        return FilterChip(
-                          label: Text(
-                            '20$year',
-                            style: TextStyle(
-                              fontSize: 11,
-                              color: selectedFilter == year
-                                  ? Colors.white
-                                  : AppColor.darkBlue,
-                            ),
-                          ),
-                          selected: selectedFilter == year,
-                          onSelected: (_) => filterQuestions(year),
-                          selectedColor: AppColor.darkBlue,
-                          showCheckmark: false,
-                          backgroundColor: Colors.grey[200],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50)),
-                        );
-                      }).toList(),
-                    ],
-                  )),
-              SizedBox(
-                width: AppMeasure.width,
-                height: AppMeasure.height / 1.4,
-                child: ListView.builder(
-                    controller: _scrollController,
-                    padding: EdgeInsets.zero,
-                    itemCount: filteredQuestions.length,
-                    itemBuilder: (context, index) {
-                      final question = filteredQuestions[index];
-                      final String uniqueKey =
-                          '${widget.topic.topic}-${question.id}';
-                      return QuestionsBubble(
-                        paperCode: widget.topic.topicCode,
-                        questionCode: question.questionCode,
-                        questionNumber: question.id,
-                        questionBody: question.questionBody,
-                        isSaved: savedQuestions.contains(uniqueKey),
-                        onSavePressed: () => _toggleSaveQuestion(
-                            question.id.toString(), widget.topic.topic),
                       );
-                    }),
-              ),
-              SizedBox(
-                height: 200,
-              )
-            ],
-          )),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () => Navigator.push(context,
-              MaterialPageRoute(builder: (context) => SavedQuestions())),
-          label: Text(
-            'Solutions',
-            style: TextStyle(
-              color: Colors.white,
+                    }).toList(),
+                  ],
+                )),
+            SizedBox(
+              width: AppMeasure.width,
+              height: AppMeasure.height / 1.4,
+              child: ListView.builder(
+                  controller: _scrollController,
+                  padding: EdgeInsets.zero,
+                  itemCount: filteredQuestions.length,
+                  itemBuilder: (context, index) {
+                    final question = filteredQuestions[index];
+                    final String uniqueKey =
+                        '${widget.topic.topic}-${question.id}';
+                    return QuestionsBubble(
+                      paperCode: widget.topic.topicCode,
+                      questionCode: question.questionCode,
+                      questionNumber: question.id,
+                      questionBody: question.questionBody,
+                      isSaved: savedQuestions.contains(uniqueKey),
+                      onSavePressed: () => _toggleSaveQuestion(
+                          question.id.toString(), widget.topic.topic),
+                    );
+                  }),
             ),
-          ),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-          backgroundColor: AppColor.darkBlue,
-          icon: Icon(
-            Icons.edit_note_outlined,
-            color: Colors.white,
-          ),
-        ));
+            SizedBox(
+              height: 200,
+            )
+          ],
+        )),
+      ),
+    );
   }
 }
