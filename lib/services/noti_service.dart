830ee4745cf68c80dfd017208e834831105eb399
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-18 23:34:04 +0200

    Created a getter method for initilization of the plugin

--- lib/services/noti_service.dart
+++ lib/services/noti_service.dart
@@ -2,4 +2,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotiService {
   final notificationPlugin = FlutterLocalNotificationsPlugin();
+
+  bool _isInitialized = false;
+  bool get isInitialized => _isInitialized;
 }
