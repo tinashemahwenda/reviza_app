833362d62b720e7a59abddcb18baa155cb3eb890
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-24 22:05:55 +0200

    Created a file for the personal details view

---
+++ lib/screens/personal_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:reviza_app/constants/app_constants.dart';
+
+class PersonalDetailsPage extends StatelessWidget {
+  const PersonalDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+    );
+  }
+}
