fca451120f5607c280203ffdbe63db5447bd8b7c
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-24 22:20:08 +0200

    Added an app bar for the page

--- lib/screens/personal_details_page.dart
+++ lib/screens/personal_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reviza_app/constants/app_constants.dart';
+import 'package:reviza_app/widgets/back_button.dart';
 
 class PersonalDetailsPage extends StatelessWidget {
   const PersonalDetailsPage({super.key});
@@ -8,6 +9,9 @@ class PersonalDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
+      body: Column(
+        children: [SettingsBackButton(pageName: 'Your Info')],
+      ),
     );
   }
 }
