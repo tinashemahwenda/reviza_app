7c9dad54292f4c5767b48d717ef7fbf2d78df9f1
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-26 23:52:40 +0200

    Removed unnecesary imports

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:lottie/lottie.dart';
 import 'package:reviza_app/constants/app_constants.dart';
 import 'package:reviza_app/screens/contact_developers_page.dart';
-import 'package:reviza_app/screens/personal_details_page.dart';
 import 'package:reviza_app/screens/saved_questions.dart';
 import 'package:reviza_app/services/noti_service.dart';
 import 'package:reviza_app/widgets/app_version.dart';
 import 'package:reviza_app/widgets/back_button.dart';
 import 'package:reviza_app/widgets/settings_tile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-//import 'package:reviza_app/widgets/go_back_button.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -52,59 +49,6 @@ class _ProfilePageState extends State<ProfilePage> {
                     ),
                     SizedBox(
                       height: 40,
-                    ),
-                    InkWell(
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PersonalDetailsPage())),
-                      child: Container(
-                        width: AppMeasure.width,
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                          spacing: 20,
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: AppColor.dodgerBlue,
-                              child: Text(
-                                _name.isNotEmpty ? _name[0] : 'X',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  _name.isNotEmpty ? _name : 'User',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                Text(
-                                  'Account Student',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                            )
-                          ],
-                        ),
-                      ),
                     ),
                     SizedBox(height: 20),
                     Container(
