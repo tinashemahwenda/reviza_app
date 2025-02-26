82534c447b2bf4c10480ec791e5dcd4ea4f9642a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-27 00:02:24 +0200

    Added variables to hold contact data

--- lib/widgets/contact_tile.dart
+++ lib/widgets/contact_tile.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ContactTile extends StatelessWidget {
-  const ContactTile({super.key});
+  final String contactNumber;
+  final IconData contactIcon;
+  const ContactTile(
+      {super.key, required this.contactNumber, required this.contactIcon});
 
   @override
   Widget build(BuildContext context) {
-    return;
+    return Row(
+      spacing: 10,
+      children: [
+        Icon(Icons.call),
+        Text('+263 773 423 888'),
+      ],
+    );
   }
 }
