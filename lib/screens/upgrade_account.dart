0affb188e02f3e84fd4ec4a7d20eff6fc346a184
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-03-01 20:24:41 +0200

    Created a file for upgrade account

---
+++ lib/screens/upgrade_account.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:reviza_app/constants/app_constants.dart';
+
+class UpgradeAccount extends StatelessWidget {
+  const UpgradeAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+    );
+  }
+}
