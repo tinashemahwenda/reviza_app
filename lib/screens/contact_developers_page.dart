1f837bcfa6d6c576ed81fe56c20d8ca1fabd978a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-03-04 23:39:45 +0200

    Fixed overflow error in the contact developers page

--- lib/screens/contact_developers_page.dart
+++ lib/screens/contact_developers_page.dart
@@ -10,164 +10,166 @@ class ContactDevelopersPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 30,
-          ),
-          child: Column(
-            children: [
-              Row(
-                spacing: 10,
-                children: [
-                  InkWell(
-                      onTap: () => Navigator.pop(context),
-                      child: Icon(Icons.arrow_back_ios)),
-                  Text(
-                    'Contact Developers',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: AppMeasure.width,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  spacing: 15,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 30,
+            ),
+            child: Column(
+              children: [
+                Row(
+                  spacing: 10,
                   children: [
+                    InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Icon(Icons.arrow_back_ios)),
                     Text(
-                      'Contact Details',
+                      'Contact Developers',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 14,
+                        fontSize: 20,
                       ),
                     ),
-                    ContactTile(
-                        contactNumber: '+263 773 423 888',
-                        contactIcon: Icons.call),
-                    ContactTile(
-                        contactNumber: 'enquiries@reviza.co.zw',
-                        contactIcon: Icons.email),
-                    ContactTile(
-                        contactNumber: 'sales@reviza.co.zw',
-                        contactIcon: Icons.mail)
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: AppMeasure.width,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  spacing: 10,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Direct Enquiry',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  width: AppMeasure.width,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    spacing: 15,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Contact Details',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Write to us....',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                          )),
-                    ),
-                    Row(
-                      children: [
-                        Spacer(),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: AppColor.darkBlue,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(
-                            'Send',
-                            style: TextStyle(
-                              color: Colors.white,
+                      ContactTile(
+                          contactNumber: '+263 773 423 888',
+                          contactIcon: Icons.call),
+                      ContactTile(
+                          contactNumber: 'enquiries@reviza.co.zw',
+                          contactIcon: Icons.email),
+                      ContactTile(
+                          contactNumber: 'sales@reviza.co.zw',
+                          contactIcon: Icons.mail)
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  width: AppMeasure.width,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Direct Enquiry',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Write to us....',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            )),
+                      ),
+                      Row(
+                        children: [
+                          Spacer(),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: AppColor.darkBlue,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              'Send',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: AppMeasure.width,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  spacing: 10,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Social Media',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
+                SizedBox(height: 20),
+                Container(
+                  width: AppMeasure.width,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Social Media',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Image.asset(
-                          'assets/images/whatsapp.png',
-                          width: 30,
-                        ),
-                        Text('Message through WhatsApp')
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Image.asset(
-                          'assets/images/facebook.png',
-                          width: 30,
-                        ),
-                        Text('Visit our Facebook Page')
-                      ],
-                    ),
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Image.asset(
-                          'assets/images/twitter.png',
-                          width: 30,
-                        ),
-                        Text('Visit our X(Twitter) Page')
-                      ],
-                    ),
-                  ],
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        spacing: 10,
+                        children: [
+                          Image.asset(
+                            'assets/images/whatsapp.png',
+                            width: 30,
+                          ),
+                          Text('Message through WhatsApp')
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        spacing: 10,
+                        children: [
+                          Image.asset(
+                            'assets/images/facebook.png',
+                            width: 30,
+                          ),
+                          Text('Visit our Facebook Page')
+                        ],
+                      ),
+                      Row(
+                        spacing: 10,
+                        children: [
+                          Image.asset(
+                            'assets/images/twitter.png',
+                            width: 30,
+                          ),
+                          Text('Visit our X(Twitter) Page')
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
