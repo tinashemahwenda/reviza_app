ae7c43bc332f13a85efd7798b6e5390fc37420b7
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-03-08 22:45:18 +0200

    Created routing for the account upgrade button

--- lib/widgets/navigationbar.dart
+++ lib/widgets/navigationbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reviza_app/screens/profile_page.dart';
+import 'package:reviza_app/screens/upgrade_account.dart';
 import '../constants/app_constants.dart';
 
 class RevizaNavigation extends StatelessWidget {
@@ -18,12 +19,16 @@ class RevizaNavigation extends StatelessWidget {
         Row(
           spacing: 10,
           children: [
-            CircleAvatar(
-              backgroundColor: AppColor.dodgerBlue,
-              radius: 24,
-              child: Icon(
-                Icons.diamond,
-                color: Colors.white,
+            InkWell(
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => UpgradeAccount())),
+              child: CircleAvatar(
+                backgroundColor: AppColor.dodgerBlue,
+                radius: 24,
+                child: Icon(
+                  Icons.diamond,
+                  color: Colors.white,
+                ),
               ),
             ),
             GestureDetector(
